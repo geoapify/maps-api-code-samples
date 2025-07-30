a991e03d7788f457085617356a64442a612ace94
Alfiya Tarasenko <alfiya.tarasenko@geoapify.com>
2025-07-30 15:25:10 +0300

    Add a link for gpx traces examples

--- javascript/map-matching-to-snap-gpx-to-roads-maplibre/combine.js
+++ javascript/map-matching-to-snap-gpx-to-roads-maplibre/combine.js
@@ -3,13 +3,13 @@ const fs = require('fs').promises;
 const path = require('path');
 
 async function inlineResources() {
-    const htmlPath = path.resolve('map-matching/src/demo.html');
-    const outputPath = path.resolve('map-matching/demo_combined.html');
+    const htmlPath = path.resolve('map-matching-to-snap-gpx-to-roads-maplibre/src/demo.html');
+    const outputPath = path.resolve('map-matching-to-snap-gpx-to-roads-maplibre/demo_combined.html');
 
     try {
         const html = await inlineSource(htmlPath, {
             compress: false,
-            rootpath: path.resolve('map-matching/src'),
+            rootpath: path.resolve('map-matching-to-snap-gpx-to-roads-maplibre/src'),
             ignore: []
         });
 
